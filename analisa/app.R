e1a5e1f4fff92ef3d27a9ea1489f69068cde7321
Muhammad Aswan Syahputra <muhammadaswansyahputra@gmail.com>
2018-04-30 10:13:17 +0200

    Menambah submodule

--- analisa/app.R
+++
@@ -1,8 +0,0 @@
-library(shiny)
-library(shinyjs)
-library(shinyWidgets)
-library(DT)
-library(stringr)
-library(tools)
-
-shinyApp(ui = ui, server = server)
--- rancang/app.R
+++
@@ -1,7 +0,0 @@
-library(shiny)
-library(DT)
-library(stringr)
-library(tools)
-library(crossdes)
-
-shinyApp(ui = ui, server = server)
